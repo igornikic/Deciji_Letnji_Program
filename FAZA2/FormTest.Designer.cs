d08933399f6f2228ae70cae87a84d52fdf89822a
kaacaa <kacanovakoviic@gmail.com>
2025-10-18 19:04:39 +0200

    test forma

---
+++ FAZA2/FormTest.Designer.cs
@@ -0,0 +1,103 @@
+﻿namespace Deciji_Letnji_Program
+{
+    partial class FormTest
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        private void InitializeComponent()
+        {
+            this.cmdUcitajDete = new System.Windows.Forms.Button();
+            this.cmdDodajDete = new System.Windows.Forms.Button();
+            this.cmdVezaDeteRoditelj = new System.Windows.Forms.Button();
+            this.cmdVezaDetePovrede = new System.Windows.Forms.Button();
+            this.cmdVezaDeteAktivnosti = new System.Windows.Forms.Button();
+            this.cmdVezaDetePrijave = new System.Windows.Forms.Button();
+            this.cmdVezaPrijavaDete = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // cmdUcitajDete
+            // 
+            this.cmdUcitajDete.Location = new System.Drawing.Point(30, 30);
+            this.cmdUcitajDete.Size = new System.Drawing.Size(250, 30);
+            this.cmdUcitajDete.Text = "Učitavanje jednog deteta";
+            this.cmdUcitajDete.Click += new System.EventHandler(this.cmdUcitajDete_Click);
+            // 
+            // cmdDodajDete
+            // 
+            this.cmdDodajDete.Location = new System.Drawing.Point(30, 70);
+            this.cmdDodajDete.Size = new System.Drawing.Size(250, 30);
+            this.cmdDodajDete.Text = "Dodavanje novog deteta";
+            this.cmdDodajDete.Click += new System.EventHandler(this.cmdDodajDete_Click);
+            // 
+            // cmdVezaDeteRoditelj
+            // 
+            this.cmdVezaDeteRoditelj.Location = new System.Drawing.Point(30, 110);
+            this.cmdVezaDeteRoditelj.Size = new System.Drawing.Size(250, 30);
+            this.cmdVezaDeteRoditelj.Text = "Veza dete–roditelj (M:N)";
+            this.cmdVezaDeteRoditelj.Click += new System.EventHandler(this.cmdVezaDeteRoditelj_Click);
+            // 
+            // cmdVezaDetePovrede
+            // 
+            this.cmdVezaDetePovrede.Location = new System.Drawing.Point(30, 150);
+            this.cmdVezaDetePovrede.Size = new System.Drawing.Size(250, 30);
+            this.cmdVezaDetePovrede.Text = "Veza dete–povrede (1:N)";
+            this.cmdVezaDetePovrede.Click += new System.EventHandler(this.cmdVezaDetePovrede_Click);
+            // 
+            // cmdVezaDeteAktivnosti
+            // 
+            this.cmdVezaDeteAktivnosti.Location = new System.Drawing.Point(30, 190);
+            this.cmdVezaDeteAktivnosti.Size = new System.Drawing.Size(250, 30);
+            this.cmdVezaDeteAktivnosti.Text = "Veza dete–aktivnosti (M:N)";
+            this.cmdVezaDeteAktivnosti.Click += new System.EventHandler(this.cmdVezaDeteAktivnosti_Click);
+            // 
+            // cmdVezaDetePrijave
+            // 
+            this.cmdVezaDetePrijave.Location = new System.Drawing.Point(30, 230);
+            this.cmdVezaDetePrijave.Size = new System.Drawing.Size(250, 30);
+            this.cmdVezaDetePrijave.Text = "Veza dete–prijava (1:N)";
+            this.cmdVezaDetePrijave.Click += new System.EventHandler(this.cmdVezaDetePrijave_Click);
+            // 
+            // cmdVezaPrijavaDete
+            // 
+            this.cmdVezaPrijavaDete.Location = new System.Drawing.Point(30, 270);
+            this.cmdVezaPrijavaDete.Size = new System.Drawing.Size(250, 30);
+            this.cmdVezaPrijavaDete.Text = "Veza prijava–dete (N:1)";
+            this.cmdVezaPrijavaDete.Click += new System.EventHandler(this.cmdVezaPrijavaDete_Click);
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(320, 340);
+            this.Controls.Add(this.cmdVezaPrijavaDete);
+            this.Controls.Add(this.cmdVezaDetePrijave);
+            this.Controls.Add(this.cmdVezaDeteAktivnosti);
+            this.Controls.Add(this.cmdVezaDetePovrede);
+            this.Controls.Add(this.cmdVezaDeteRoditelj);
+            this.Controls.Add(this.cmdDodajDete);
+            this.Controls.Add(this.cmdUcitajDete);
+            this.Name = "Form1";
+            this.Text = "Deciji Letnji Program – NHibernate test";
+            this.ResumeLayout(false);
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button cmdUcitajDete;
+        private System.Windows.Forms.Button cmdDodajDete;
+        private System.Windows.Forms.Button cmdVezaDeteRoditelj;
+        private System.Windows.Forms.Button cmdVezaDetePovrede;
+        private System.Windows.Forms.Button cmdVezaDeteAktivnosti;
+        private System.Windows.Forms.Button cmdVezaDetePrijave;
+        private System.Windows.Forms.Button cmdVezaPrijavaDete;
+    }
+}
---
+++ FAZA2/FormTest.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Linq;
+using System.Windows.Forms;
+using NHibernate;
+using Deciji_Letnji_Program.Entiteti;
+
+namespace Deciji_Letnji_Program
+{
+    public partial class FormTest : Form
+    {
+        public FormTest()
+        {
+            InitializeComponent();
+        }
+
+        // 1. Učitavanje jednog deteta
+        private void cmdUcitajDete_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                using (ISession s = DataLayer.GetSession())
+                {
+                    Dete d = s.Load<Dete>(1); 
+                    MessageBox.Show($"Dete: {d.Ime} {d.Prezime}, adresa: {d.Adresa}");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Greška: " + ex.Message);
+            }
+        }
+
+        // 2. Dodavanje novog deteta
+        private void cmdDodajDete_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                using (ISession s = DataLayer.GetSession())
+                {
+                    Dete d = new Dete()
+                    {
+                        Ime = "Petar",
+                        Prezime = "Petrović",
+                        Pol = 'M',
+                        DatumRodjenja = new DateTime(2012, 5, 10),
+                        Adresa = "Bulevar Oslobođenja 15",
+                        TelefonDeteta = "061123456",
+                        EmailDeteta = "petar.petrovic@example.com",
+                        PosebnePotrebe = "Nema"
+                    };
+
+                    s.Save(d);
+                    s.Flush();
+                    MessageBox.Show($"Novo dete je sačuvano sa ID = {d.ID}");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Greška: " + ex.Message);
+            }
+        }
+
+        // 3. Veza dete–roditelj (M:N)
+        private void cmdVezaDeteRoditelj_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                using (ISession s = DataLayer.GetSession())
+                {
+                    Dete d = s.Load<Dete>(1);
+                    MessageBox.Show($"Dete: {d.Ime} {d.Prezime}");
+
+                    foreach (var r in d.Roditelji)
+                    {
+                        MessageBox.Show($"Roditelj: {r.Ime} {r.Prezime}");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Greška: " + ex.Message);
+            }
+        }
+
+        // 4. Veza dete–povrede (1:N)
+        private void cmdVezaDetePovrede_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                using (ISession s = DataLayer.GetSession())
+                {
+                    Dete d = s.Load<Dete>(1);
+                    MessageBox.Show($"Dete: {d.Ime} {d.Prezime}");
+
+                    foreach (var p in d.Povrede)
+                    {
+                        MessageBox.Show($"Povreda ID={p.ID}, opis: {p.Opis}, datum: {p.Datum.ToShortDateString()}");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Greška: " + ex.Message);
+            }
+        }
+
+        // 5. Veza dete–aktivnosti (M:N preko Ucestvuje)
+        private void cmdVezaDeteAktivnosti_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                using (ISession s = DataLayer.GetSession())
+                {
+                    Dete d = s.Load<Dete>(1);
+                    MessageBox.Show($"Dete: {d.Ime} {d.Prezime}");
+
+                    foreach (var u in d.Ucestvuje)
+                    {
+                        MessageBox.Show($"Aktivnost: {u.Aktivnost?.Naziv}, Prisustvo: {u.Prisustvo}, Ocena: {u.OcenaAktivnosti}");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Greška: " + ex.Message);
+            }
+        }
+
+        // 6. Veza dete–prijava (1:N)
+        private void cmdVezaDetePrijave_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                using (ISession s = DataLayer.GetSession())
+                {
+                    Dete d = s.Load<Dete>(1);
+                    MessageBox.Show($"Dete: {d.Ime} {d.Prezime}");
+
+                    foreach (var p in d.Prijave)
+                    {
+                        MessageBox.Show($"Prijava ID={p.IdPrijave}, datum: {p.DatumPrijave.ToShortDateString()}, status: {p.Status}");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Greška: " + ex.Message);
+            }
+        }
+
+        // 7. Veza prijava–dete (N:1)
+        private void cmdVezaPrijavaDete_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                using (ISession s = DataLayer.GetSession())
+                {
+                    Prijava p = s.Load<Prijava>(1);
+                    MessageBox.Show($"Prijava ID={p.IdPrijave}, status: {p.Status}");
+
+                    if (p.Dete != null)
+                        MessageBox.Show($"Ova prijava pripada detetu: {p.Dete.Ime} {p.Dete.Prezime}");
+                    else
+                        MessageBox.Show("Ova prijava nema povezano dete!");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Greška: " + ex.Message);
+            }
+        }
+    }
+}
