5eccdd2139a2ec51e3184eac7e2b2b86729fd494
igornikic <igor.nikic.dev@gmail.com>
2025-10-26 17:10:48 +0100

    done

--- FAZA3/OracleWebAPIService/Controllers/PovredaController.cs
+++ FAZA3/OracleWebAPIService/Controllers/PovredaController.cs
@@ -42,38 +42,6 @@ namespace OracleWebAPIService.Controllers
             return Ok(povreda);
         }
 
-        //[HttpPost]
-        //[Route("DodajPovredu")]
-        //[ProducesResponseType(StatusCodes.Status201Created)]
-        //[ProducesResponseType(StatusCodes.Status400BadRequest)]
-        //[ProducesResponseType(StatusCodes.Status404NotFound)]
-        //public async Task<IActionResult> DodajPovredu([FromBody] PovredaPregled povreda)
-        //{
-        //    (bool isError, bool ok, var error) = await DataProvider.AddPovredaAsync(povreda);
-
-        //    if (isError)
-        //        return StatusCode(error?.StatusCode ?? 400, error?.Message);
-
-        //    return StatusCode(201, "Povreda je uspešno dodata.");
-        //}
-
-
-        //[HttpPut]
-        //[Route("AzurirajPovredu")]
-        //[ProducesResponseType(StatusCodes.Status200OK)]
-        //[ProducesResponseType(StatusCodes.Status400BadRequest)]
-        //[ProducesResponseType(StatusCodes.Status404NotFound)]
-        //[ProducesResponseType(StatusCodes.Status500InternalServerError)]
-        //public async Task<IActionResult> AzurirajPovredu([FromBody] PovredaPregled povreda)
-        //{
-        //    (bool isError, bool ok, var error) = await DataProvider.UpdatePovredaAsync(povreda);
-
-        //    if (isError)
-        //        return StatusCode(error?.StatusCode ?? 400, error?.Message);
-
-        //    return Ok("Povreda je uspešno ažurirana.");
-        //}
-
         [HttpPost]
         [Route("DodajPovredu/{aktivnostId}/{deteId}/{odgovornoLiceJMBG}")]
         [ProducesResponseType(StatusCodes.Status201Created)]
